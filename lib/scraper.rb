a0b762edfc27eb7f1a787cdd9f6c467985d12678
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-18 23:07:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,13 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_page_url)
-    NOKOGIRI::HTML(open(index_page_url))
+    page = NOKOGIRI::HTML(open(index_page_url))
      students = []
      page.css("h4.student-name")
      binding.pry
   end
 
-  # def self.scrape_profile_page(profile_url)
+   def self.scrape_profile_page(profile_url)
     
-  # end
+   end
 end
