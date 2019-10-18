46f5f9d889ab098d3be40fa9b8b1a47f8a7ea2c4
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-18 22:37:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,16 @@ require "nokogiri"
 
 class Scraper
   
+  attr_accessor :name, :location, :profile_url
   
+  @@all = []
+  
+  def initialize(name,location,profile_url)
+    @name = name
+    @location = location
+    @profile_url = profile_url
+    @@all << self
+  end
 
   def self.scrape_index_page(index_page_url)
      
